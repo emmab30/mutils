501c93af95a12c755407581d858f7abf3a990df1
System Administrator <eabuslaiman@gmail.com>
2023-02-18 20:03:03 +0100

    Implemented new examples on web3 and updated the readme file

--- src/__tests__/web3.test.ts
+++ src/__tests__/web3.test.ts
@@ -32,21 +32,117 @@ test('Querying router contract from provider', async () => {
 test('Querying factory contract from provider', async () => {
     const provider = web3Utils.Provider.getProviderByChainId(1);
     const factory = web3Utils.Contract.getFactoryFromProvider(provider);
-    const pair = await factory.getPair('0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2');
+    const pair = await factory.getPair(
+        '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce',
+        '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    );
     expect(web3Utils.address(pair)).toBe(web3Utils.address('0x811beed0119b4afce20d2583eb608c6f7af1954f'));
 });
 
 test('Querying token contract', async () => {
     const provider = web3Utils.Provider.getProviderByChainId(1);
-    const token = web3Utils.Contract.getTokenContractFromProvider('0x811beEd0119b4AfCE20D2583EB608C6F7AF1954f', provider);
-    let balance = await token.balanceOf('0x4527106ae1A661A9D2Ffc22575baCdaaCb5e51e0')
+    const token = web3Utils.Contract.getTokenContractFromProvider(
+        '0x811beEd0119b4AfCE20D2583EB608C6F7AF1954f',
+        provider,
+    );
+    let balance = await token.balanceOf('0x4527106ae1A661A9D2Ffc22575baCdaaCb5e51e0');
     balance = ethers.utils.formatUnits(balance, 18);
     expect(parseFloat(balance)).toBeGreaterThan(0);
 });
 
 test('Querying custom contract (no token, no router, no factory)', async () => {
     const provider = web3Utils.Provider.getProviderByChainId(1);
-    const contract = web3Utils.Contract.getContractFromAddress('0x5e227ad1969ea493b43f840cff78d08a6fc17796', [{"constant":false,"inputs":[{"components":[{"internalType":"address","name":"target","type":"address"},{"internalType":"bytes","name":"callData","type":"bytes"}],"internalType":"struct Multicall.Call[]","name":"calls","type":"tuple[]"}],"name":"aggregate","outputs":[{"internalType":"uint256","name":"blockNumber","type":"uint256"},{"internalType":"bytes[]","name":"returnData","type":"bytes[]"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"blockNumber","type":"uint256"}],"name":"getBlockHash","outputs":[{"internalType":"bytes32","name":"blockHash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockCoinbase","outputs":[{"internalType":"address","name":"coinbase","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockDifficulty","outputs":[{"internalType":"uint256","name":"difficulty","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockGasLimit","outputs":[{"internalType":"uint256","name":"gaslimit","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getCurrentBlockTimestamp","outputs":[{"internalType":"uint256","name":"timestamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"addr","type":"address"}],"name":"getEthBalance","outputs":[{"internalType":"uint256","name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getLastBlockHash","outputs":[{"internalType":"bytes32","name":"blockHash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"}], provider);
-    let currentBlockGasLimit = await contract.getCurrentBlockGasLimit()
+    const contract = web3Utils.Contract.getContractFromAddress(
+        '0x5e227ad1969ea493b43f840cff78d08a6fc17796',
+        [
+            {
+                constant: false,
+                inputs: [
+                    {
+                        components: [
+                            { internalType: 'address', name: 'target', type: 'address' },
+                            { internalType: 'bytes', name: 'callData', type: 'bytes' },
+                        ],
+                        internalType: 'struct Multicall.Call[]',
+                        name: 'calls',
+                        type: 'tuple[]',
+                    },
+                ],
+                name: 'aggregate',
+                outputs: [
+                    { internalType: 'uint256', name: 'blockNumber', type: 'uint256' },
+                    { internalType: 'bytes[]', name: 'returnData', type: 'bytes[]' },
+                ],
+                payable: false,
+                stateMutability: 'nonpayable',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [{ internalType: 'uint256', name: 'blockNumber', type: 'uint256' }],
+                name: 'getBlockHash',
+                outputs: [{ internalType: 'bytes32', name: 'blockHash', type: 'bytes32' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'getCurrentBlockCoinbase',
+                outputs: [{ internalType: 'address', name: 'coinbase', type: 'address' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'getCurrentBlockDifficulty',
+                outputs: [{ internalType: 'uint256', name: 'difficulty', type: 'uint256' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'getCurrentBlockGasLimit',
+                outputs: [{ internalType: 'uint256', name: 'gaslimit', type: 'uint256' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'getCurrentBlockTimestamp',
+                outputs: [{ internalType: 'uint256', name: 'timestamp', type: 'uint256' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [{ internalType: 'address', name: 'addr', type: 'address' }],
+                name: 'getEthBalance',
+                outputs: [{ internalType: 'uint256', name: 'balance', type: 'uint256' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+            {
+                constant: true,
+                inputs: [],
+                name: 'getLastBlockHash',
+                outputs: [{ internalType: 'bytes32', name: 'blockHash', type: 'bytes32' }],
+                payable: false,
+                stateMutability: 'view',
+                type: 'function',
+            },
+        ],
+        provider,
+    );
+    let currentBlockGasLimit = await contract.getCurrentBlockGasLimit();
     expect(parseFloat(currentBlockGasLimit)).toBeGreaterThan(0);
 });
--- src/modules/w3/provider.ts
+++ src/modules/w3/provider.ts
@@ -1,85 +1,73 @@
 const { ethers } = require('ethers');
 
 export interface IProvider {
-    chainId: number,
-    rpcs: string[],
+    chainId: number;
+    rpcs: string[];
     // Optional data
-    name?: string,
-    explorer?: string,
-    router?: string,
-    factory?: string,
-    factory_abi?: string,
-    router_abi?: string,
-    multicall?: string
-};
+    name?: string;
+    explorer?: string;
+    router?: string;
+    factory?: string;
+    factory_abi?: string;
+    router_abi?: string;
+    multicall?: string;
+}
 
 export const createProvider = (provider: IProvider): IProvider => {
     return provider;
-}
+};
 
 /*
-*   Get provider by chain id
-*   @returns provider or undefined if not found
-*/
+ *   Get provider by chain id
+ *   @returns provider or undefined if not found
+ */
 export const getProviderByChainId = (chainId: number): IProvider | undefined => {
     return Providers.find((provider: IProvider) => provider.chainId === chainId);
-}
+};
 
 const Providers: IProvider[] = [
     createProvider({
         name: 'Ethereum Mainnet',
         explorer: 'https://etherscan.io',
         chainId: 1,
-        rpcs: [
-            'https://rpc.ankr.com/eth',
-            'https://cloudflare-eth.com'
-        ],
+        rpcs: ['https://rpc.ankr.com/eth', 'https://cloudflare-eth.com'],
         router: '0x7a250d5630b4cf539739df2c5dacb4c659f2488d',
         factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
         factory_abi: require('./abis/erc20.factory.json'),
         router_abi: require('./abis/erc20.router.json'),
-        multicall: '0xcA11bde05977b3631167028862bE2a173976CA11'
+        multicall: '0xcA11bde05977b3631167028862bE2a173976CA11',
     }),
     createProvider({
         name: 'Goerli Testnet',
         explorer: 'https://goerli.etherscan.io',
         chainId: 5,
-        rpcs: [
-            'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161	',
-            'https://rpc.ankr.com/eth_goerli'
-        ],
+        rpcs: ['https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161	', 'https://rpc.ankr.com/eth_goerli'],
         router: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
         factory: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
         factory_abi: require('./abis/erc20.factory.json'),
         router_abi: require('./abis/erc20.router.json'),
-        multicall: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e'
+        multicall: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e',
     }),
     createProvider({
         name: 'Binance Smart Chain',
         explorer: 'https://bscscan.com',
         chainId: 56,
-        rpcs: [
-            'https://bsc-dataseed.binance.org/',
-            'https://bsc-dataseed1.defibit.io/'
-        ],
+        rpcs: ['https://bsc-dataseed.binance.org/', 'https://bsc-dataseed1.defibit.io/'],
         router: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
         factory: '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73',
         factory_abi: require('./abis/erc20.factory.json'),
         router_abi: require('./abis/erc20.router.json'),
-        multicall: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb'
+        multicall: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb',
     }),
     createProvider({
         name: 'Binance Smart Chain Testnet',
         explorer: 'https://testnet.bscscan.com',
         chainId: 97,
-        rpcs: [
-            'https://data-seed-prebsc-1-s1.binance.org:8545/',
-            'https://data-seed-prebsc-2-s1.binance.org:8545/'
-        ],
+        rpcs: ['https://data-seed-prebsc-1-s1.binance.org:8545/', 'https://data-seed-prebsc-2-s1.binance.org:8545/'],
         router: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
         factory: '0x6725F303b657a9451d8BA641348b6761A6CC7a17',
         factory_abi: require('./abis/erc20.factory.json'),
         router_abi: require('./abis/erc20.router.json'),
-        multicall: '0x6e5bb1a5ad6f68a8d7d6a5e47750ec15773d6042'
-    })
+        multicall: '0x6e5bb1a5ad6f68a8d7d6a5e47750ec15773d6042',
+    }),
 ];
